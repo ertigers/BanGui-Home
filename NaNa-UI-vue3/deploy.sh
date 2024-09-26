da76d0a1cbdc4f0aa0a3376f90cc202e40976a31
lvyihu <1720427782@qq.com>
2024-09-26 17:07:46 +0800

    修改线上更新的代码

--- NaNa-UI-vue3/deploy.sh
+++ NaNa-UI-vue3/deploy.sh
@@ -1,11 +1,11 @@
 rm -rf dist &&
-yarn build &&
+npm run build &&
 cd dist &&
 git init &&
 git add . &&
 git commit -m "update" &&
 git branch -M main &&
-git remote add origin git@github.com:ertigers/nana-ui-website.git &&
+git remote add origin git@github.com:ertigers/Website.git &&
 git push -f -u origin main &&
 cd -
-echo https://ertigers.github.io/nana-ui-website/index.html
+echo https://ertigers.github.io/Website/nana-ui/index.html
